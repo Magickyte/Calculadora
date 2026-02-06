bf5230d7f8953d231f2148d443757a8214dcb45c
Magickyte <a377043@uach.mx>
2026-02-05 18:13:34 -0600

    Operacion de Suma

---
+++ src/main/java/com/fing/app/Controllers/Calc.java
@@ -0,0 +1,28 @@
+package com.fing.app.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Calc {
+	
+	
+	//"calc/{oper}/{n1}/{n2}"
+	@GetMapping("/calc/{oper}/{n1}/{n2}")
+	public String calc(@PathVariable String oper, @PathVariable int n1,@PathVariable int n2) {
+		if(oper.equals("suma")) {
+			return "La suma de " +n1 +"+"+n2+"="+(n1+n2);
+		}else
+			return "Operacion invalida";
+	}
+	
+	@RequestMapping("/")
+	public String root() {
+		
+		return "Hola";
+	}
+	
+}
